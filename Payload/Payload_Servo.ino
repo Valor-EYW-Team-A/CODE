a7c4ccbecbfa1fdc7c5788caef4b6d550ba4e866
maxwellely05 <maxwell.ely05@gmail.com>
2017-05-11 10:14:53 -0700

    chnaged 10 second timer to 15 second timer

---
+++ Payload/Payload_Servo.ino
@@ -0,0 +1,23 @@
+#include <EYW.h> // access library that includes all needed commands 
+
+EYW:: Camera payloadservo; // names servo payloadservo 
+
+void setup()
+{
+payloadservo.begin(); 
+payloadservo.calibrate();
+                                  //after callibration it will sound an alarm then a a 10 second *changed to 15 second timer will commence 
+payloadservo.alarm();
+payloadservo.beginTimer(15000);
+}
+
+void loop() 
+{
+  if (payloadservo.timerExpired()==true||payloadservo.buttonPressed()==true);
+  {
+                                                          //camera needs to take pictures either before timer expires or botton is pressed, a timer of 5 seconds wiil begin, it will repeat inifinitly 
+    payloadservo.getPicture();
+    payloadservo.beginTimer(5000);
+  }
+
+}
