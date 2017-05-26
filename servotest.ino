738a3322f5b65f3fe21bbf1f1f6bb894e3669b60
maxwellely05 <maxwell.ely05@gmail.com>
2017-05-26 10:42:35 -0700

    Add files via upload
    Mr. Neils edits, plus getting the correct angle for servo push.

---
+++ servotest.ino
@@ -0,0 +1,46 @@
+#include <EYW.h>
+#include <Servo.h>
+#include <Wire.h>
+
+EYW::Camera myservo;
+
+unsigned long timerstart=0;
+unsigned long duration=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.begin();
+  myservo.calibrate();
+  beginTimer(10000);
+  myservo.alarm(3, 400, 300);
+  myservo.motor.write(90);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(checkTimer()==true){
+    myservo.alarm(2, 1200, 200);
+    myservo.motor.write(150);
+    delay(500);
+    myservo.motor.write(100);
+    beginTimer(3000);
+  }
+}
+
+void beginTimer(int dur)
+{
+  timerstart = millis();
+  duration = dur;
+}
+
+bool checkTimer()
+{
+  if ((millis()-timerstart) > duration){
+    return true;
+  } else {
+    return false;
+  }
+}
+
